cee050eb1ac90085fd34aa90fb32238e2f2b3fbb
camelenchon <camelenchon@hotmail.com>
2024-03-15 23:30:46 -0300

    lista 4 - lacos de repeticao

---
+++ Listas/Lista 4/Exercicio1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int contadorTabuada = 1; contadorTabuada <= 10; contadorTabuada++)
+        {
+            for (int contador = 1; contador <= 10; contador++)
+            {
+                System.Console.WriteLine($"{contadorTabuada} X {contador} = {contadorTabuada * contador}");
+            }
+            System.Console.WriteLine();
+        }
+
+    }
+}
---
+++ Listas/Lista 4/Exercicio2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int ALTURA_MAXIMA_PERMITIDA = 9;
+        const int ALTURA_MINIMA_PERMITIDA = 1;
+        int alturaRecebida = 0;
+
+        do
+        {
+            System.Console.WriteLine("Digite um tamanho de triangulo entre 1 e 9");
+            alturaRecebida = int.Parse(Console.ReadLine());
+
+            if (alturaRecebida > ALTURA_MAXIMA_PERMITIDA || alturaRecebida < ALTURA_MINIMA_PERMITIDA)
+            {
+                System.Console.WriteLine("Altura invalida, digite um numero entre 1 a 9!!!");
+            }
+            else
+            {
+                for (int linha = 1; linha <= alturaRecebida; linha++)
+                {
+                    for (int coluna = 1; coluna <= linha; coluna++)
+                    {
+                        Console.Write(coluna);
+                    }
+                    System.Console.WriteLine();
+                }
+
+            }
+        }
+        while (alturaRecebida > ALTURA_MAXIMA_PERMITIDA || alturaRecebida < ALTURA_MINIMA_PERMITIDA);
+    }
+
+}
