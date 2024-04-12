7ac1b27c844edb34dd36124e5be0ce93dac3a981
camelenchon <camelenchon@hotmail.com>
2024-04-12 19:52:48 -0300

    Structs

---
+++ Listas/Lista 8/Exercicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+struct Pizza
+{
+    public int id;
+    public string sabor;
+    public double preco;
+
+}
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Pizza pizza;
+        pizza.id = Convert.ToInt32(Console.ReadLine());
+        pizza.sabor = Console.ReadLine();
+        pizza.preco = Convert.ToDouble(Console.ReadLine());
+
+        System.Console.WriteLine(pizza.id);
+        System.Console.WriteLine(pizza.sabor);
+        System.Console.WriteLine(pizza.preco);
+
+    }
+}
---
+++ Listas/Lista 8/Exercicio2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+struct Produto
+{
+    public int id;
+    public string nome;
+    public double valor;
+    public bool disponivelEmEstoque;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produto[] produto = new Produto[4];
+
+        for (int i = 0; i < produto.Length; i++)
+        {
+            produto[i].id = Convert.ToInt32(Console.ReadLine());
+            produto[i].nome = Console.ReadLine();
+            produto[i].valor = Convert.ToDouble(Console.ReadLine());
+            produto[i].disponivelEmEstoque = bool.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < produto.Length; i++)
+        {
+            if (produto[i].disponivelEmEstoque == true)
+            {
+                System.Console.WriteLine(produto[i].nome);
+                System.Console.WriteLine(Math.Round(produto[i].valor, 2));
+            }
+        }
+    }
+
+}
